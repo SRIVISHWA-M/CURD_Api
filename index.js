6b7fc4b2187c556f2c6a94b3a2f86cd9a0c6183c
M.Srivishwa <142809256+SRIVISHWA-M@users.noreply.github.com>
2025-07-01 08:28:51 +0530

    dding Configuration

--- index.js
+++ index.js
@@ -2,21 +2,61 @@ const express = require('express')
 const mongoose = require('mongoose')
 const Product = require('./models/productModel')
 const app = express()
-
+// middleware
 app.use(express.json())
+
+
 app.get('/',(req,res) => {
   res.send('hii hello')
 })
-
-app.post('/products',async (req,res) => {
+// Add-prod
+app.post('/api/products',async (req,res) => {
+  // console.log("Received:", req.body);
   try {
     const product = await Product.create(req.body)
     res.status(200).json(product)
   } catch (error) {
     res.status(500).json({message:error.message})
   }
+  // console.log(req.body);
+  // res.send(req.body)
 })
-CURD.connect("mongodb://localhost:27017/CURD")
+// Reviwe the all-prod
+app.get('/api/products',async (req,res) => {
+  try {
+    const product = await Product.find({})
+    res.status(200).json(product)
+  } catch (error) {
+    res.status(500).json({message:error.message})
+  }
+})
+// review using id
+app.get('/api/product/:id',async (req,res) => {
+  try {
+  const product = await Product.findById(req.params.id)
+  if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+  res.status(200).json(product)
+  } catch (error) {
+    res.status(500).json({message:error.message})
+  }
+})
+// Update prod
+app.put('/api/product/:id',async (req,res) => {
+  try {
+    const {id} =req.params
+    const product =await Product.findByIdAndUpdate(req.body)
+    if (!product) {
+      return res.status(404).json({message:"Product not found"})
+    }
+    const updateProduct = await Product.findById(id)
+    res.status(200).json(updateProduct)
+  } catch (error) {
+    res.status(500).json({message:error.message})
+  }
+})
+mongoose.connect("mongodb://localhost:27017/CURD")
 .then(() => {
   console.log("DB is connected");
   app.listen(2222,() =>{
--- models/productModel.js
+++ models/productModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const ProductSchema = mongoose.Schema(
   {
-    name:{type:string,required:[true,"name is important"],unique:true},
+    name:{type:String,required:[true,"name is important"]},
     price:{type:Number,required:[true,"price is important"],default:0},
     quantity:{type:Number,required:[true,"quantity is important"],default:0}
   },{
